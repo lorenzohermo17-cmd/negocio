92a0b2a5b2b01a27e2a6fbc576f2bb91f56c31ad
lolo <lorenzohermo17@gmail.com>
2025-10-24 20:58:02 -0300

    Agregamos metodo anadir stock

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
+
+    def add_stock(self,stock:int) -> str:
+        if stock <= 0:
+            return "No se puede agregar 0 o menos, que te pensas que sos"
+
+        self.stock += stock
+
+        return "Se cambio con exito el stock"
+
+producto = Producto("Remera",40000,0)
+
+producto.add_stock()
